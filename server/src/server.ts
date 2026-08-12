770714ab1a34c94d19e26e1b96cbe0f87efceaec
Chitranshu5497 <chitranshu5497@gmail.com>
2026-08-13 01:11:19 +0530

    Prepare server for production deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,7 @@ import app from "./app";
 import { createServer } from "http";
 import { Server } from "socket.io";
 
-import {
-  initializeSocket
-} from "./socket/socket";
+import { initializeSocket } from "./socket/socket";
 
 // BullMQ workers
 import "./jobs/workers/invite.worker";
@@ -12,28 +10,17 @@ import "./jobs/workers/password-reset.worker";
 
 const httpServer = createServer(app);
 
-
-const io = new Server(
-  httpServer,
-  {
-    cors:{
-      origin:"http://localhost:5173",
-      credentials:true,
-    },
-  }
-);
-
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
+});
 
 initializeSocket(io);
 
+const PORT = Number(process.env.PORT) || 5000;
 
-
-httpServer.listen(
-  5000,
-  ()=>{
-    console.log(
-      "Server running on 5000"
-    );
-    
-  }
-);
+httpServer.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${PORT}`);
+});
