f4a4ebfd25c470686d62489db8947520e21a49fb
Chitranshu5497 <chitranshu5497@gmail.com>
2026-08-13 14:23:39 +0530

    Fix production Socket.IO connection

--- client/src/socket/socket.ts
+++ client/src/socket/socket.ts
@@ -1,10 +1,9 @@
 import { io } from "socket.io-client";
 
-
 export const socket = io(
-  "http://localhost:5000",
+  import.meta.env.VITE_SOCKET_URL,
   {
-    autoConnect:false,
-    withCredentials:true
+    autoConnect: false,
+    withCredentials: true,
   }
 );
